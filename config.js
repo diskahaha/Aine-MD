116cb22787aa661bf5cf2b948cce414bcbe9310c
diskahaha <109210020+diskahaha@users.noreply.github.com>
2022-07-14 12:57:37 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,9 +2,8 @@ let fs = require('fs')
 let chalk = require('chalk')
 
 global.owner = [
-  ['62895330379186'],
-  ['62895330379186'],
-  ['62895330379186', 'Owner', true]
+  ['6281285578808'],
+  ['12055512826'], 'Owner', true]
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
 global.mods = [] // Want some help?
@@ -40,8 +39,8 @@ process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0
 const spack = fs.readFileSync("lib/exif.json")
 const stickerpack = JSON.parse(spack)
 if (stickerpack.spackname == '') {
-  var sticker_name = '@ainee_bot'
-  var sticker_author = 'Aine'
+  var sticker_name = '@𝕭𝖔𝖙 • Andii ༢࿔ྀ'
+  var sticker_author = '𝕭𝖔𝖙 • Andii ༢࿔ྀ'
 } else {
   var sticker_name = stickerpack.spackname
   var sticker_author = stickerpack.sauthor
@@ -58,7 +57,7 @@ fs.watchFile(file_exif, () => {
 // Sticker WM
 global.packname = sticker_name
 global.author = sticker_author
-global.wm = '© Aine'
+global.wm = '© 𝕭𝖔𝖙 • Andii ༢࿔ྀ'
 
 global.multiplier = 1000 // The higher, The harder levelup
 
